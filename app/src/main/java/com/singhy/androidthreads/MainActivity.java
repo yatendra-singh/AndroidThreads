b6c07803f9a65c00fc465b38fc150fe92e54b2ae
Yatendra Singh <yatendra.singh@autodesk.com>
2018-05-19 23:47:33 +0530

    Initial Commit - UI Thread- ANR

---
+++ app/src/main/java/com/singhy/androidthreads/MainActivity.java
@@ -0,0 +1,58 @@
+package com.singhy.androidthreads;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private Button buttonStart, buttonStop;
+    int count = 0;
+    private boolean mStopLoop;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Thread id of Main thread: " + Thread.currentThread().getId());
+
+        buttonStart = (Button) findViewById(R.id.buttonThreadStarter);
+        buttonStop = (Button) findViewById(R.id.buttonStopthread);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonThreadStarter:
+                mStopLoop = false;
+
+                while (!mStopLoop) {
+                    try {
+                        Thread.sleep(1000);
+                        count++;
+                    } catch (InterruptedException e) {
+                        Log.i(TAG, e.getMessage());
+                    }
+                    Log.i(TAG, "Thread id in while loop: " + Thread.currentThread().getId() + ", Count : " + count);
+                }
+                break;
+
+            case R.id.buttonStopthread:
+                mStopLoop = true;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
